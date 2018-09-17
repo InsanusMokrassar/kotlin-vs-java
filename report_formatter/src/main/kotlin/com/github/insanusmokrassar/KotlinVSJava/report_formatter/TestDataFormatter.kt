ce8a610cbc336528bf634225b97da4b0c30d91fd
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2018-09-17 09:46:15 +0800

    complete printPoints

--- report_formatter/src/main/kotlin/com/github/insanusmokrassar/KotlinVSJava/report_formatter/TestDataFormatter.kt
+++ report_formatter/src/main/kotlin/com/github/insanusmokrassar/KotlinVSJava/report_formatter/TestDataFormatter.kt
@@ -1,7 +1,16 @@
 package com.github.insanusmokrassar.KotlinVSJava.report_formatter
 
 fun printPoints(points: List<LogPoint>) {
-    points.forEach {
-        println()
+    val sortedPoints = points.sortedBy { it.dateTime }
+    var previousPoint: LogPoint? = null
+    sortedPoints.forEach {
+        point ->
+        previousPoint ?.also {
+            previousPoint ->
+            println("${point.testName}:${point.dateTime.minus(previousPoint.dateTime.millis).millis} ms:${point.message}")
+        } ?: point.also {
+            println("first:${point.testName}:${point.dateTime}:${point.message}")
+        }
+        previousPoint = point
     }
 }
