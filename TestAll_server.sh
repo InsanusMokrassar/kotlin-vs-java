1df96ed4861f5e2508e9dda3405b70b37b3fa708
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2019-03-29 16:16:56 +0800

    rename and update main tests files

--- TestAll_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./HelloWorld.sh -n 100
-./QuickSort.sh -n 100 -a 100000
-./ContactsBook.sh -n 100 -jvm-args -Xmx2048m -a "50 1000"
-./ImageHandling.sh -n 10 -a "`pwd`/test_sprite_images/"
-./NetworkEmulation.sh -n 3 -jvm-args -Xmx2048m -a 100000
